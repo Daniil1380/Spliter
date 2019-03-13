d3e9f419759ff7bb32ff77cc466aced7ecc69729
Daniil1380 <Tkachenko1380@yandex.ru>
2019-03-13 20:57:15 +0300

    Доделаны тесты, программа готова к проверке )

--- src/test/java/SeparatorTest.java
+++ src/test/java/SeparatorTest.java
@@ -9,6 +9,7 @@ public class SeparatorTest {
 
     @Test
     public void l() throws IOException {
+        Separator.l("TestText.txt", 100, "OutputFile\\file", true);
         File file1 = new File("OutputFile\\file1.txt");
         File fileTest1 = new File("TestFile\\fileTest11.txt");
         FileReader read1 = new FileReader(file1);
@@ -32,14 +33,15 @@ public class SeparatorTest {
 
     @Test
     public void c() throws IOException {
-        File file1 = new File("OutputFile\\file1.txt");
-        File fileTest1 = new File("TestFile\\fileTest11.txt");
+        Separator.c("TestText.txt", 90, "OutputFile\\file1", true);
+        File file1 = new File("OutputFile\\file11.txt");
+        File fileTest1 = new File("TestFile\\fileTest21.txt");
         FileReader read1 = new FileReader(file1);
         BufferedReader buf1 = new BufferedReader(read1);
         FileReader read2 = new FileReader(fileTest1);
         BufferedReader buf2 = new BufferedReader(read2);
         String line1 = buf1.readLine();
-        boolean test=true;
+        boolean test = true;
         String line2 = buf2.readLine();
         while (line1 != null || line2 != null) {
             if (!line1.equals(line2)) {
@@ -49,11 +51,30 @@ public class SeparatorTest {
             line1 = buf1.readLine();
             line2 = buf2.readLine();
         }
-
         Assert.assertTrue(test);
     }
 
     @Test
-    public void n() {
+    public void n() throws IOException {
+        Separator.n("TestText.txt", 2, "OutputFile\\file2", false);
+        File file1 = new File("OutputFile\\file2a.txt");
+        File fileTest1 = new File("TestFile\\fileTest2a.txt");
+        FileReader read1 = new FileReader(file1);
+        BufferedReader buf1 = new BufferedReader(read1);
+        FileReader read2 = new FileReader(fileTest1);
+        BufferedReader buf2 = new BufferedReader(read2);
+        String line1 = buf1.readLine();
+        boolean test = true;
+        String line2 = buf2.readLine();
+        while (line1 != null || line2 != null) {
+            if (!line1.equals(line2)) {
+                test = false;
+                break;
+            }
+            line1 = buf1.readLine();
+            line2 = buf2.readLine();
+        }
+        Assert.assertTrue(test);
     }
-}
+
+    }
